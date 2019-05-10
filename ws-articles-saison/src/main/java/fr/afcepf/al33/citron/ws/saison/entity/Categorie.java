59e62ef1639113b78a75e584167c5d62f382e578
Regis <regis@provider.com>
2019-05-10 17:36:41 +0200

    ajout @Transactional avec @Service

--- ws-articles-saison/src/main/java/fr/afcepf/al33/citron/ws/saison/entity/Categorie.java
+++ ws-articles-saison/src/main/java/fr/afcepf/al33/citron/ws/saison/entity/Categorie.java
@@ -8,6 +8,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -23,6 +27,8 @@ import lombok.Setter;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 @Entity
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)//pour que @XmlTRansient soit interprété sur du private.
 public class Categorie {
 	
 	@Id
@@ -32,6 +38,7 @@ public class Categorie {
 	private String nom;
 	
 	@JsonIgnore
+	@XmlTransient
 	@OneToMany(mappedBy="categorie",fetch=FetchType.LAZY)
 	//mappedBy="nom java de la relation inverse manyToOne"
 	private List<Article> articles;	
--- ws-articles-saison/src/main/java/fr/afcepf/al33/citron/ws/saison/ws/ServiceClientArticleSaisonImpl.java
+++ ws-articles-saison/src/main/java/fr/afcepf/al33/citron/ws/saison/ws/ServiceClientArticleSaisonImpl.java
@@ -3,6 +3,7 @@ package fr.afcepf.al33.citron.ws.saison.ws;
 import java.util.List;
 
 import javax.jws.WebService;
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ import fr.afcepf.al33.citron.ws.saison.entity.Article;
 import fr.afcepf.al33.citron.ws.saison.entity.Categorie;
 
 @Service  //de Spring
+@Transactional
 @WebService(endpointInterface="fr.afcepf.al33.citron.ws.saison.ws.ServiceClientArticleSaison")
 public class ServiceClientArticleSaisonImpl implements ServiceClientArticleSaison {
 	
